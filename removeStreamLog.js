1a4e8d9eada3a8b7487baebcb403e7d893ff54ef
Khalipha91 <108478050+Khalipha91@users.noreply.github.com>
2022-07-01 10:32:00 +0200

    Add files via upload

---
+++ removeStreamLog.js
@@ -0,0 +1,53 @@
+"use strict";
+const AWS = require("aws-sdk");
+
+let options = {};
+
+if (process.env.IS_OFFLINE) {
+  options = {
+    region: "localhost",
+    endpoint: "http://localhost:8000",
+  };
+}
+
+const db = new AWS.DynamoDB.DocumentClient(options);
+
+const userStreams = process.env.USER_STREAMS_TABLE;
+
+const response = (statusCode, message) => {
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify({
+      statusCode: statusCode,
+      message: message,
+    }),
+  };
+};
+
+module.exports.removeStream = (event, context, callback) => {
+  const requestBody = JSON.parse(event.body);
+
+  const params = {
+    TableName: userStreams,
+    Key: {
+      streamId: requestBody.streamId,
+    },
+    ConditionExpression: "#uid = :uid",
+    ExpressionAttributeNames: {
+      "#uid": "userId",
+    },
+    ExpressionAttributeValues: {
+      ":uid": requestBody.userId,
+    },
+  };
+
+  return db
+    .delete(params)
+    .promise()
+    .then(() => {
+      callback(null, response(204));
+    })
+    .catch((error) => {
+      callback(null, response(404, { error: error.message }));
+    });
+};
